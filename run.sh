1b38f0504686577e3931dd95f12fbdb565c80e24
joeyism <sham.joey@gmail.com>
2017-09-13 17:43:19 -0400

    Updated to add .vimrc and .tmux.conf files

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+wget https://raw.githubusercontent.com/joeyism/.files/master/.tmux.conf ~/
+wget https://raw.githubusercontent.com/joeyism/.files/master/.vimrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
