d667fb371d257eeff6cdd70716146cba58430c6a
Joey Sham <sham.joey@gmail.com>
2017-11-17 09:44:37 -0500

    Create configure_general.sh

---
+++ configure_general.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/joeyism/.files/master/.vimrc 
+wget https://raw.githubusercontent.com/joeyism/.files/master/.tmux.conf
+yum install tmux
+sudo yum install git 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ '" >> .bashrc
+. ~/.bashrc
