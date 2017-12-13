afcbc50b777e77ae97ba28b095f9ed12d57d67fd
Joey Sham <sham.joey@gmail.com>
2017-12-12 20:58:19 -0500

    Update yum-python-instances.sh

--- instances/yum-python-instances.sh
+++ instances/yum-python-instances.sh
@@ -2,7 +2,7 @@ sudo yum install -y gcc
 sudo yum install -y python35
 sudo yum install -y python35-setuptools
 sudo easy_install-3.5 pip
-sudo pip-3.4 install ipython
+pip3 install --user ipython
 wget https://raw.githubusercontent.com/joeyism/.files/master/configure_general.sh
 bash configure_general.sh
 rm configure_general.sh
