87cc3018225b1726e26023b195afbf65da370147
Joey Sham <sham.joey@gmail.com>
2017-12-12 23:58:49 -0500

    Update apt-opencv-python-instances-2.sh

--- instances/apt-opencv-python-instances-2.sh
+++ instances/apt-opencv-python-instances-2.sh
@@ -5,7 +5,7 @@ unzip opencv_contrib.zip
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
-sudo apt-get install build-essential cmake pkg-config
+sudo apt-get install build-essential cmake pkg-config virtualenv
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install libxvidcore-dev libx264-dev libgtk-3-dev
 sudo apt-get install libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev
