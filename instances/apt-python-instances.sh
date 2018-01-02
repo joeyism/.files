5644660dfcd44f999d584c3307eaefe82c780092
Joey Sham <sham.joey@gmail.com>
2018-01-02 18:49:07 -0500

    Create apt-python-instances.sh

---
+++ instances/apt-python-instances.sh
@@ -0,0 +1,5 @@
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python3 get-pip.py
+wget https://raw.githubusercontent.com/joeyism/.files/master/configure_general.sh
+bash configure_general.sh
+rm configure_general.sh
