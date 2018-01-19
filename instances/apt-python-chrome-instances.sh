411b526a44776ba90bf05f446504cc9bb91a23ae
Joey Sham <sham.joey@gmail.com>
2018-01-19 18:52:35 -0500

    Update apt-python-chrome-instances.sh

--- instances/apt-python-chrome-instances.sh
+++ instances/apt-python-chrome-instances.sh
@@ -2,7 +2,7 @@ wget https://raw.githubusercontent.com/joeyism/.files/master/instances/apt-pytho
 bash apt-python-instances.sh
 rm apt-python-instances.sh
 
-sudo apt install -y libnss3 gconf2
+sudo apt install -y libnss3 gconf2 xvfb 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
 sudo apt-get install -f
