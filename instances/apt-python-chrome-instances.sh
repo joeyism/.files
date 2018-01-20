d1136706d6b69cd49619c96af1993266a3c66afa
Joey Sham <sham.joey@gmail.com>
2018-01-20 16:31:13 -0500

    Update apt-python-chrome-instances.sh

--- instances/apt-python-chrome-instances.sh
+++ instances/apt-python-chrome-instances.sh
@@ -5,7 +5,7 @@ rm apt-python-instances.sh
 sudo apt install -y libnss3 gconf2 xvfb 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
-sudo apt-get install -f
+sudo apt-get install -f -y
 rm google-chrome-stable_current_amd64.deb
 
 echo "export DISPLAY=:0.0" >> ~/.bashrc
