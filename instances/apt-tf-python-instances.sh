9bf84f615beae6a21b8935b0cbf20731347f3dfd
Joey Sham <sham.joey@gmail.com>
2018-02-06 15:48:06 -0500

    Update apt-tf-python-instances.sh

--- instances/apt-tf-python-instances.sh
+++ instances/apt-tf-python-instances.sh
@@ -9,21 +9,22 @@ sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
 sudo apt-get install -y libxvidcore-dev libx264-dev libgtk-3-dev
 sudo apt-get install -y libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev
 sudo apt-get install -y libatlas-base-dev numpy gfortran qt5-default htop
-sudo apt-get install -y python3.5-dev
+sudo apt-get install -y python3.5-dev python3.5-tk 
 sudo apt-get -y install python3-pip
-pip3 install --user ipython numpy
+pip3 install --user ipython numpy cython h5py keras imageio
 pip3 install --ignore-installed --upgrade "https://github.com/lakshayg/tensorflow-build/raw/master/tensorflow-1.4.0-cp35-cp35m-linux_x86_64.whl"
 
 virtualenv cv
 cd cv
 source bin/activate
 
-git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg
-cd ffmpeg
-./configure --enable-nonfree --enable-pic --enable-shared
-make
-sudo make install
+python3 -c "import imageio; imageio.plugins.ffmpeg.download()"
+#git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg
+#cd ffmpeg
+#./configure --enable-nonfree --enable-pic --enable-shared
+#make
+#sudo make install
 
-wget https://raw.githubusercontent.com/joeyism/.files/master/configure_general.sh
-bash configure_general.sh
-rm configure_general.sh
+wget https://raw.githubusercontent.com/joeyism/.files/master/run.sh
+bash run.sh
+rm run.sh
