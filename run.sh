b7ec88bdf1cd8c94c314da7b05dfc4030988bc0d
Joey Sham <sham.joey@gmail.com>
2018-02-28 09:58:02 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@ wget https://raw.githubusercontent.com/joeyism/.files/master/.tmux.conf ~/
 wget https://raw.githubusercontent.com/joeyism/.files/master/.vimrc ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+echo 'export PROMPT_COMMAND="history -a; history -n"' >> .bashrc
