8d33c3071e3ee4bd6896a93c83084f83b2305ec2
Joey Sham <sham.joey@gmail.com>
2018-03-01 20:16:51 -0500

    Update apt-tf-gpu-python-instances.sh

--- instances/apt-tf-gpu-python-instances.sh
+++ instances/apt-tf-gpu-python-instances.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # IF GPU, remove anaconda part
-AMI_ID=$(curl http://169.254.169.254/latest/meta-data/ami-id)
-if [ $AMI_ID == "ami-eb596e8e" ]
-  then
-    head -n -1 ~/.bashrc > ~/.bashrc
-fi
-source ~/.bashrc
+#AMI_ID=$(curl http://169.254.169.254/latest/meta-data/ami-id)
+#if [ $AMI_ID == "ami-eb596e8e" ]
+#then
+#    head -n -1 ~/.bashrc > ~/.bashrc
+#fi
+#source ~/.bashrc
 
 sudo apt-get -y update
 sudo apt-get upgrade -y
