826263dffb134240a03d77c949ea1419fc8015f5
Joey Sham <sham.joey@gmail.com>
2018-03-13 15:39:18 -0400

    Update yum-python-pyspark-instance.sh

--- instances/yum-python-pyspark-instance.sh
+++ instances/yum-python-pyspark-instance.sh
@@ -13,6 +13,10 @@ sudo easy_install-3.5 pip
 sudo cp /usr/local/bin/pip3 /usr/sbin/
 pip3 install --user ipython
 
+# Java8
+sudo yum install -y java-1.8.0-openjdk
+sudo yum remove -y java-1.7.0-openjdk
+
 # Spark
 cd /opt
 sudo wget http://www-eu.apache.org/dist/spark/spark-2.2.1/spark-2.2.1-bin-hadoop2.7.tgz
@@ -21,7 +25,6 @@ sudo ln -s /opt/spark-2.2.1-bin-hadoop2.7 /opt/spark
 
 export PYSPARK_PYTHON=python3 
 export PYSPARK_DRIVER_PYTHON=ipython 
-export PYSPARK_DRIVER_PYTHON_OPTS="notebook" 
 export SPARK_HOME=/opt/spark
 export PATH=$SPARK_HOME/bin:$PATH
 
@@ -32,4 +35,4 @@ export PATH=$SPARK_HOME/bin:$PATH
 wget https://raw.githubusercontent.com/joeyism/.files/master/configure_general.sh
 bash configure_general.sh
 rm configure_general.sh
-printf '\nexport SPARK_HOME=/opt/spark\nexport PATH=$SPARK_HOME/bin:$PATHPYSPARK_PYTHON=python3\nPYSPARK_DRIVER_PYTHON=ipython\nPYSPARK_DRIVER_PYTHON_OPTS="notebook"' >> ~/.bash_rc
+printf '\nexport SPARK_HOME=/opt/spark\nexport PATH=$SPARK_HOME/bin:$PATHPYSPARK_PYTHON=python3\nPYSPARK_DRIVER_PYTHON=ipython\n' >> ~/.bash_rc
