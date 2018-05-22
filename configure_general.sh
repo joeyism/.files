131d85247ce9da4efee90b9af9e9af22dbfdd288
Joey Sham <sham.joey@gmail.com>
2018-05-22 13:39:39 -0400

    Update configure_general.sh

--- configure_general.sh
+++ configure_general.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/joeyism/.files/master/.vimrc 
-wget https://raw.githubusercontent.com/joeyism/.files/master/.tmux.conf
 sudo yum install -y tmux
 sudo yum install -y git 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+wget -O - https://raw.githubusercontent.com/joeyism/.files/master/run.sh | bash -
 echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ '" >> .bashrc
 . ~/.bashrc
