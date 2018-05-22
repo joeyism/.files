4491049980e527205dc4d2367031cce903502400
Joey Sham <sham.joey@gmail.com>
2018-05-22 13:39:28 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,6 @@ wget https://raw.githubusercontent.com/joeyism/.files/master/.aliases ~/
 source ~/.aliases
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ '" >> .bashrc
 echo 'export PROMPT_COMMAND="history -a; history -n"' >> .bashrc
+
