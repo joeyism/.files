29528d362aa76bebb136e509942bab62767a018e
Joey Sham <sham.joey@gmail.com>
2018-08-03 13:25:29 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/joeyism/.files/master/.tmux.conf ~/
 wget https://raw.githubusercontent.com/joeyism/.files/master/.vimrc ~/
-wget https://raw.githubusercontent.com/joeyism/.files/master/.aliases ~/
-source ~/.aliases
+wget https://raw.githubusercontent.com/joeyism/.files/master/.bash_aliases ~/
+source ~/.bash_aliases
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 echo "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ '" >> .bashrc
