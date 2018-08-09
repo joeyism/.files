45351a36dda0ead5b70633e85212143e94208f72
Joey Sham <sham.joey@gmail.com>
2018-08-09 15:51:34 -0400

    Create apt-python-get-instances.sh

---
+++ instances/apt-python-get-instances.sh
@@ -0,0 +1,5 @@
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install pip3 htop dialog nethogs
+
+wget -O - https://raw.githubusercontent.com/joeyism/.files/master/configure_general.sh | bash -
