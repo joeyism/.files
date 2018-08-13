25d40728a768f9025f7cf72df7dd770e78df689a
Joey Sham <sham.joey@gmail.com>
2018-08-13 12:06:57 -0400

    Update apt-tf-gpu-python-instances-2.sh

--- instances/apt-tf-gpu-python-instances-2.sh
+++ instances/apt-tf-gpu-python-instances-2.sh
@@ -1,6 +1,6 @@
 export python_version=35
+wget -O - https://raw.githubusercontent.com/joeyism/.files/master/run_apt.sh | bash -
 sudo apt -y install gcc make python3-pip
 wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run
 sudo sh cuda_9.0.176_384.81_linux-run --override
 sudo pip3 install https://github.com/mind/wheels/releases/download/tf1.7-gpu-nomkl/tensorflow-1.7.0-cp${python_version}-cp${python_version}m-linux_x86_64.whl
-wget -O - https://raw.githubusercontent.com/joeyism/.files/master/run_apt.sh | bash -
