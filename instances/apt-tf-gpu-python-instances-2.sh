17e40943a091c8b55e553c787e9cdd2ad0d56259
Joey Sham <sham.joey@gmail.com>
2018-08-13 14:50:42 -0400

    Update apt-tf-gpu-python-instances-2.sh

--- instances/apt-tf-gpu-python-instances-2.sh
+++ instances/apt-tf-gpu-python-instances-2.sh
@@ -7,7 +7,8 @@ rm run_apt.sh
 
 # Install dependencies (gcc-6, g++-6, etc.)
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt -y install gcc-6 make python3-pip build-essential libelf-dev g++-6
+sudo apt -y install gcc-6 make python3-pip build-essential libelf-dev g++-6 nvidia-384 nvidia-modprobe
+
 
 # Let gcc to be gcc-6
 sudo rm /usr/bin/gcc /usr/bin/gcc-ar /usr/bin/gcc-nm /usr/bin/gcc-ranlib
@@ -19,5 +20,6 @@ sudo ln -s /usr/bin/g++-6 /usr/bin/g++
 
 # Install CUDA
 wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run
-sudo sh cuda_9.0.176_384.81_linux-run --override
+sudo sh cuda_9.0.176_384.81_linux-run --override --extract=$HOME
+sudo bash cuda-linux.9.0.176-22781540.run
 sudo pip3 install https://github.com/mind/wheels/releases/download/tf1.7-gpu-nomkl/tensorflow-1.7.0-cp${python_version}-cp${python_version}m-linux_x86_64.whl
