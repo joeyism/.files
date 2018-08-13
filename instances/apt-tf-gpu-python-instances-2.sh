632bda3a08be4d9002783ab01c92333078df783a
Joey Sham <sham.joey@gmail.com>
2018-08-13 14:25:39 -0400

    Update apt-tf-gpu-python-instances-2.sh

--- instances/apt-tf-gpu-python-instances-2.sh
+++ instances/apt-tf-gpu-python-instances-2.sh
@@ -1,11 +1,12 @@
 export python_version=35
 
+# My stuff
 wget https://raw.githubusercontent.com/joeyism/.files/master/run_apt.sh
 bash run_apt.sh
 rm run_apt.sh
 
+# Install dependencies (gcc-6, g++-6, etc.)
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-
 sudo apt -y install gcc-6 make python3-pip build-essential libelf-dev g++-6
 
 # Let gcc to be gcc-6
@@ -16,6 +17,7 @@ sudo ln -s /usr/bin/gcc-nm-6 /usr/bin/gcc-nm
 sudo ln -s /usr/bin/gcc-ranlib-6 /usr/bin/gcc-ranlib
 sudo ln -s /usr/bin/g++-6 /usr/bin/g++
 
+# Install CUDA
 wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run
 sudo sh cuda_9.0.176_384.81_linux-run --override
 sudo pip3 install https://github.com/mind/wheels/releases/download/tf1.7-gpu-nomkl/tensorflow-1.7.0-cp${python_version}-cp${python_version}m-linux_x86_64.whl
