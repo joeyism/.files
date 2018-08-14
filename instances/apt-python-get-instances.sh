2f5693115e0ee35475f998757c009a67f94b434b
Joey Sham <sham.joey@gmail.com>
2018-08-14 12:23:02 -0400

    Update apt-python-get-instances.sh

--- instances/apt-python-get-instances.sh
+++ instances/apt-python-get-instances.sh
@@ -2,4 +2,4 @@ sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y install python3-pip htop dialog nethogs
 
-wget -O - https://raw.githubusercontent.com/joeyism/.files/master/configure_general.sh | bash -
+wget -O - https://raw.githubusercontent.com/joeyism/.files/master/run.sh | bash -
