fa3258b20c81a992823786b84ac6f7b57dedaa0d
Joey Sham <sham.joey@gmail.com>
2018-12-19 00:33:40 -0500

    Update apt-tf-gpu-python-instances-3.sh

--- instances/apt-tf-gpu-python-instances-3.sh
+++ instances/apt-tf-gpu-python-instances-3.sh
@@ -30,7 +30,7 @@ mv cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb cuda-repo-ubuntu1604-9-0-l
 sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64.deb
 sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub 
 sudo apt-get update
-sudo apt-get install -y cuda cuda-*-9-0
+sudo apt-get install -y cuda-*-9-0
 
 sudo pip3 install --upgrade https://github.com/mind/wheels/releases/download/tf1.7-gpu-nomkl/tensorflow-1.7.0-cp${python_version}-cp${python_version}m-linux_x86_64.whl
 sudo pip3 install gpustat
