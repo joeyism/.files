1d879386e9ce6c942c732bafe0896dacf96cf6e4
Joey Sham <sham.joey@gmail.com>
2019-05-17 14:44:51 -0400

    Update network.sh

--- .bash_scripts/network.sh
+++ .bash_scripts/network.sh
@@ -8,3 +8,8 @@ alias wifi_device_enable="ip link set wlp3s0 up"
 alias wifi_iw_status="iw dev wlp3s0 link"
 alias enable_dhcpcd="systemctl status dhcpcd"
 alias scan_local_ssh="nmap 192.168.0.1/24 -p 22 --open"
+ssh_remote_port(){
+    read -p "Local Port: " local_port
+    read -p "Remote Port: " remote_port
+    ssh -L $local_port:localhost:$remote_port $@
+}
